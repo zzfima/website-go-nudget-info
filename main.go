c2b9c98247dcdb0a0dfb184f267819e9eecebcaa
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-11-17 09:47:13 +0200

    show versions

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"time"
 
 	nugetInfo "github.com/zzfima/Golang-Nuget-info"
 )
@@ -13,8 +15,8 @@ func main() {
 	startServer()
 }
 
-func welcomeHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome to Nuget page. Show all versions of Nuget MvvmCross")
+func versionsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Show all versions of Nuget MvvmCross")
 	fmt.Fprintf(w, "\n")
 	versions, _ := nugetInfo.GetNugetVersions("MvvmCross")
 	for _, version := range versions {
@@ -23,7 +25,35 @@ func welcomeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// HomePageMessage ...
+type HomePageMessage struct {
+	Message     string
+	CurrentTime string
+}
+
+// VersionsPageMessage ...
+type VersionsPageMessage struct {
+	Message     string
+	CurrentTime string
+	Versions    []string
+}
+
+func homePageHandler(w http.ResponseWriter, r *http.Request) {
+	homePageTemplate, _ := template.ParseFiles("templates/home_page.html")
+	homePageMsg := HomePageMessage{"Welcome to Nuget page Information", time.Now().Format("2006-01-02 15:04:05")}
+	homePageTemplate.Execute(w, homePageMsg)
+
+	versionsPageTemplate, _ := template.ParseFiles("templates/versions_page.html")
+	versions, _ := nugetInfo.GetNugetVersions(r.FormValue("nugetName"))
+	versionsPageMsg := VersionsPageMessage{
+		"Welcome to Versions page Information", time.Now().Format("2006-01-02 15:04:05"),
+		versions}
+
+	versionsPageTemplate.Execute(w, versionsPageMsg)
+}
+
 func startServer() {
-	http.HandleFunc("/", welcomeHandler)
+	http.HandleFunc("/", homePageHandler)
+	http.HandleFunc("/versions", versionsHandler)
 	http.ListenAndServe(":8080", nil)
 }
