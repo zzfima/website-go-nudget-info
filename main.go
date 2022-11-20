d519f65bdcc621a6c7ed9836bd159a259799183c
Efim Zabarsky <Efim.Zabarsky@kla-tencor.com>
2022-11-20 21:30:12 +0200

    gorilla mux works sucks

--- main.go
+++ main.go
@@ -6,19 +6,11 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/gorilla/mux"
 	nugetInfo "github.com/zzfima/Golang-Nuget-info"
 )
 
-var router = mux.NewRouter()
-
 func main() {
 	fmt.Println("web page for Nuget")
-
-	router.HandleFunc("/", homePageHandler).Methods("GET")
-	router.HandleFunc("/versions", versionsPageHandler).Methods("GET")
-	router.HandleFunc("/metadata", metadataPageHandler).Methods("GET")
-
 	startServer()
 }
 
@@ -37,8 +29,7 @@ func versionsPageHandler(w http.ResponseWriter, r *http.Request) {
 	versionsPageTemplate, _ := template.ParseFiles("templates/versions_page.html")
 
 	if r.ContentLength != 0 {
-		vars := mux.Vars(r)
-		versions, _ := nugetInfo.GetNugetVersions(vars["nugetName"])
+		versions, _ := nugetInfo.GetNugetVersions(r.FormValue("nugetName"))
 		versionsPageMsg := VersionsPageMessage{versions}
 		versionsPageTemplate.Execute(w, versionsPageMsg)
 	} else {
@@ -50,8 +41,7 @@ func metadataPageHandler(w http.ResponseWriter, r *http.Request) {
 	versionsPageTemplate, _ := template.ParseFiles("templates/metadata_page.html")
 
 	if r.ContentLength != 0 {
-		vars := mux.Vars(r)
-		metadata, _ := nugetInfo.GetNugetMetadata(vars["nugetName"], vars["nugetVersion"])
+		metadata, _ := nugetInfo.GetNugetMetadata(r.FormValue("nugetName"), r.FormValue("nugetVersion"))
 		versionsPageTemplate.Execute(w, metadata)
 	} else {
 		versionsPageTemplate.Execute(w, nil)
@@ -68,5 +58,9 @@ func homePageHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func startServer() {
-	http.ListenAndServe(":8080", router)
+	http.HandleFunc("/", homePageHandler)
+	http.HandleFunc("/versions", versionsPageHandler)
+	http.HandleFunc("/metadata", metadataPageHandler)
+
+	http.ListenAndServe(":8080", nil)
 }
